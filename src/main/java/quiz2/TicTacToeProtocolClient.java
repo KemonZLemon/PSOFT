632eb21534d114c6b27609e8746130ea9c344110
Kevin <zhukevinapp@gmail.com>
2026-01-28 21:59:23 -0500

    another test

--- src/main/java/quiz2/TicTacToeProtocolClient.java
+++ src/main/java/quiz2/TicTacToeProtocolClient.java
@@ -1,9 +1,7 @@
 /**
- *
+ * 
  */
-package quiz2;
-
-import quiz2.Connectable;
+package edu.rpi.csci2600.gildem4;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -24,66 +22,66 @@ import java.util.logging.StreamHandler;
  *
  */
 public class TicTacToeProtocolClient implements Connectable {
-    public TicTacToeProtocolClient(String server) {
-        this(server, DEFAULT_PORT);
-    }
-
-    public TicTacToeProtocolClient(String server, int port) {
-        this.log = Logger.getLogger("global");
-        this.server = server;
-        this.port = port;
-    }
-
-    @Override
-    public void connect() throws IOException {
-        this.socket = new Socket(this.server, this.port);
-
-        log.info(String.format("Connection to server %s established at port %d.\n", server, port));
-        this.inStream =  this.socket.getInputStream();
-        this.outStream = this.socket.getOutputStream();
-        this.in = new Scanner(this.inStream);
-        this.out = new PrintWriter(new OutputStreamWriter(this.outStream, StandardCharsets.UTF_8), true /*autoFlush */);
-    }
-
-    @Override
-    public void send(String message) {
-        this.out.println(message);
-        log.info(String.format("Message %s sent.\n", message));
-    }
-
-    @Override
-    public String receive() {
-        String message = this.in.nextLine();
-        log.info(String.format("Message %s received.\n", message));
-        return message;
-    }
-
-    public int getPort() {
-        return this.port;
-    }
-
-    public String getServer() {
-        return this.server;
-    }
-
-    public boolean isConnectionClosed() {
-        return this.socket.isClosed();
-    }
-
-    public static final int DEFAULT_PORT = 8189;
-
-    private Socket socket;
-    private String server;
-    private int port;
-    private InputStream inStream;
-    private OutputStream outStream;
-    Scanner in;
-    PrintWriter out;
-    private Logger log;
+	public TicTacToeProtocolClient(String server) {
+		this(server, DEFAULT_PORT);		
+	}
+	
+	public TicTacToeProtocolClient(String server, int port) {
+		this.log = Logger.getLogger("global");
+		this.server = server;
+		this.port = port;
+	}
+	
+	@Override
+	public void connect() throws IOException {
+		this.socket = new Socket(this.server, this.port);
+		
+		log.info(String.format("Connection to server %s established at port %d.\n", server, port));
+		this.inStream =  this.socket.getInputStream();
+		this.outStream = this.socket.getOutputStream();
+		this.in = new Scanner(this.inStream);
+		this.out = new PrintWriter(new OutputStreamWriter(this.outStream, StandardCharsets.UTF_8), true /*autoFlush */);		
+	}
+	
+	@Override
+	public void send(String message) {
+		this.out.println(message);
+		log.info(String.format("Message %s sent.\n", message));
+	}
+	
+	@Override
+	public String receive() {
+		String message = this.in.nextLine();
+		log.info(String.format("Message %s received.\n", message));
+		return message;
+	}
+	
+	public int getPort() {
+		return this.port;
+	}
+	
+	public String getServer() {
+		return this.server;
+	}
+	
+	public boolean isConnectionClosed() {
+		return this.socket.isClosed();
+	}
+	
+	public static final int DEFAULT_PORT = 8189;
+	
+	private Socket socket;
+	private String server;
+	private int port;
+	private InputStream inStream;
+	private OutputStream outStream;
+	Scanner in;
+	PrintWriter out;
+	private Logger log;	
 }
 
 class TicTacToeProtocolClientDemo {
-    /*
+	/*
     To test:
     1. java edu.rpi.csci2600.u25.kuzmik2.TicTacToeProtocolServerTest 8189
     2. java edu.rpi.csci2600.u25.kuzmik2.TicTacToeProtocolClientTest localhost 8189
@@ -98,64 +96,64 @@ class TicTacToeProtocolClientDemo {
     11.mark O 0 4
     12.mark X 4 4
  */
-    public static void main(String[] args) throws IOException, UnknownHostException {
-        Logger log = Logger.getLogger("global");
-        StreamHandler handler;
-        try {
-            handler = new FileHandler();
-        }
-        catch (Exception e) {
-            log.warning(String.format("Unable to create global logger file handler: %s", e.getMessage()));
-            throw e;
-        }
-        handler.setFormatter(new SimpleFormatter());
-        log.addHandler(handler);
-
-        TicTacToeProtocolClient gameComm = null;
-        try {
-            if (args.length == 0) {
-                log.severe("Remote server name has to be specified in the command line arguments");
-                return;
-            }
-            else if (args.length > 1) {
-                gameComm = new TicTacToeProtocolClient(args[0], Integer.parseInt(args[1]));
-            }
-            else if (args.length > 0) {
-                gameComm = new TicTacToeProtocolClient(args[0]);
-            }
-        }
-        catch (NumberFormatException e) {
-            if (gameComm != null ) {
-                gameComm = new TicTacToeProtocolClient(args[0]);
-                log.warning(String.format("Invalid port number %s, using the default value of %d", args[1], gameComm.getPort()));
-            }
-        }
-        try {
-            gameComm.connect();
-        }
-        catch (UnknownHostException e) {
-            log.severe(String.format("Unknown host: %s.\n", e.getMessage()));
-            throw e;
-        }
-        catch (IOException e) {
-            log.severe(String.format("Unable to create socket: %s", e.getMessage()));
-            throw e;
-        }
-
-        String command, response = "";
-        try (Scanner in = new Scanner(System.in);) {
-            while (!gameComm.isConnectionClosed() && response.indexOf("wins") == -1 && response.indexOf("draw") == -1)
-            {
-                System.out.println("Enter your command:");
-                command = in.nextLine();
-                gameComm.send(command);
-                log.info(String.format("Command: %s sent.", command));
-                response = gameComm.receive();
-                log.info(String.format("Response: %s received.", response));
-            }
-        }
-        catch (NoSuchElementException e) {
-            log.info(String.format("Unable to read next line from the input: %s", e.getMessage()));
-        }
-    }
+	public static void main(String[] args) throws IOException, UnknownHostException {
+		Logger log = Logger.getLogger("global");
+		StreamHandler handler;
+		try {
+			handler = new FileHandler();
+		}
+		catch (Exception e) {
+			log.warning(String.format("Unable to create global logger file handler: %s", e.getMessage()));
+			throw e;
+		} 
+		handler.setFormatter(new SimpleFormatter());
+		log.addHandler(handler);
+		
+		TicTacToeProtocolClient gameComm = null;
+		try {
+			if (args.length == 0) {
+				log.severe("Remote server name has to be specified in the command line arguments");
+				return;
+			}
+			else if (args.length > 1) {
+				gameComm = new TicTacToeProtocolClient(args[0], Integer.parseInt(args[1]));
+			}
+			else if (args.length > 0) {
+				gameComm = new TicTacToeProtocolClient(args[0]);
+			}
+		}
+		catch (NumberFormatException e) {
+			if (gameComm != null ) {
+				gameComm = new TicTacToeProtocolClient(args[0]);	
+				log.warning(String.format("Invalid port number %s, using the default value of %d", args[1], gameComm.getPort()));
+			}
+		}
+		try {
+			gameComm.connect();
+		}
+		catch (UnknownHostException e) {
+			log.severe(String.format("Unknown host: %s.\n", e.getMessage()));
+			throw e;
+		} 
+		catch (IOException e) {
+			log.severe(String.format("Unable to create socket: %s", e.getMessage()));
+			throw e;
+		}
+		
+		String command, response = "";
+		try (Scanner in = new Scanner(System.in);) {
+			while (!gameComm.isConnectionClosed() && response.indexOf("wins") == -1 && response.indexOf("draw") == -1)
+			{
+				System.out.println("Enter your command:");
+				command = in.nextLine();
+				gameComm.send(command);
+				log.info(String.format("Command: %s sent.", command));
+				response = gameComm.receive();
+				log.info(String.format("Response: %s received.", response));
+			}
+		}
+		catch (NoSuchElementException e) {
+			log.info(String.format("Unable to read next line from the input: %s", e.getMessage()));
+		}
+	}
 }
